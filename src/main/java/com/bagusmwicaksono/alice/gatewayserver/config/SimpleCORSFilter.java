1da30374011e8ff65b455d26b0f158e03e6c1311
Bagus <bagusmwicaksono@gmail.com>
2020-03-18 11:31:44 +0800

    Gateway Server another Initial Commit

---
+++ src/main/java/com/bagusmwicaksono/alice/gatewayserver/config/SimpleCORSFilter.java
@@ -0,0 +1,48 @@
+package com.bagusmwicaksono.alice.gatewayserver.config;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+public class SimpleCORSFilter implements Filter {
+    public static final String CREDENTIALS_NAME = "Access-Control-Allow-Credentials";
+    public static final String ORIGIN_NAME = "Access-Control-Allow-Origin";
+    public static final String METHODS_NAME = "Access-Control-Allow-Methods";
+    public static final String HEADERS_NAME = "Access-Control-Allow-Headers";
+    public static final String MAX_AGE_NAME = "Access-Control-Max-Age";
+
+    private FilterConfig config;
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        config=filterConfig;
+    }
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) resp;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "x-requested-with, authorization, Content-Type, Authorization, credential, X-XSRF-TOKEN");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            chain.doFilter(req, resp);
+        }
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
---
+++ src/test/java/com/bagusmwicaksono/alice/gatewayserver/GatewayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bagusmwicaksono.alice.gatewayserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
